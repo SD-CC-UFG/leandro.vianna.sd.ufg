a558004747b1bea7863d3597745f738ef5ce890f
leandrovianna <leandro97vianna@gmail.com>
2018-08-28 20:29:32 -0300

    arrumando bugs e terminando chat

--- chat/client.go
+++ chat/client.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"os"
 )
 
 const PORT = 7777
 
 func readMessages() {
-	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", PORT))
+	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", PORT))
 
 	if err != nil {
-		fmt.Printf("ERROR: %v\n", err)
+		log.Fatal("ERROR: %v\n", err)
 		return
 	}
 
@@ -19,22 +22,35 @@ func readMessages() {
 
 	buffer := make([]byte, 4096)
 
-	conn.Write([]byte("VIEW"))
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	writer.Write([]byte("VIEW\n"))
+	writer.Flush()
 
 	for {
-		n, err := conn.Read(buffer)
+		n, err := reader.Read(buffer)
 
 		if err != nil {
-			fmt.Printf("ERROR: %v\n", err)
+			log.Printf("ERROR: %v\n", err)
 			break
+		}
+
+		response := string(buffer[:n])
+
+		if response == "ERR" {
+			log.Printf("ERROR: Server response with error.\n")
+			return
 		}
 
 		fmt.Println(string(buffer[:n]))
 	}
 }
 
-func writeMessages(name string) {
-	var msg string
+func writeMessages(name string, exitChan chan bool) {
+	defer func() {
+		exitChan <- true
+	}()
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", PORT))
 
@@ -45,18 +61,45 @@ func writeMessages(name string) {
 
 	defer conn.Close()
 
-	conn.Write([]byte("JOIN"))
+	buffer := make([]byte, 4096)
 
-	conn.Write([]byte(name))
-	fmt.Printf("Send name and JOIN\n")
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
+
+	writer.Write([]byte(fmt.Sprintf("JOIN %s\n", name)))
+	writer.Flush()
+
+	var msg string
+
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Scanln(&msg)
-		conn.Write([]byte(msg))
+		n, err := reader.Read(buffer)
+
+		if err != nil {
+			log.Fatal("Erro fatal: %v\n", err)
+		}
+
+		response := string(buffer[:n])
+
+		if response == "ERR" {
+			log.Fatal("Erro fatal: Servidor respondeu com erro.\n")
+		}
+
+		scanner.Scan()
+		msg = scanner.Text()
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal("Erro fatal: %v\n", err)
+		}
 
 		if msg == "quit" {
+			fmt.Printf("Tchau!\n")
 			break
 		}
+
+		writer.Write([]byte(fmt.Sprintf("MSG %s <end>\n", msg)))
+		writer.Flush()
 	}
 }
 
@@ -67,6 +110,10 @@ func main() {
 		fmt.Scan(&name)
 	}
 
-	go writeMessages(name)
-	readMessages()
+	exitChan := make(chan bool)
+
+	go writeMessages(name, exitChan)
+	go readMessages()
+
+	<-exitChan
 }
--- chat/server.go
+++ chat/server.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type Message struct {
 	SenderName string
 	Text       string
+	Time       time.Time
+}
+
+func (m Message) String() string {
+	return fmt.Sprintf("[%s] %s: %s\n", m.Time.Format("15:04:05"), m.SenderName, m.Text)
 }
 
 func NewMessage(name, msg string) Message {
-	message := Message{name, msg}
+	message := Message{name, msg, time.Now()}
 	return message
 }
 
@@ -30,7 +38,7 @@ func AddMessage(name, msg string) {
 }
 
 func listenClients(port int) error {
-	log.Printf("Listening for connections in port %d\n", port)
+	log.Printf("Esperando conexões na porta %d\n", port)
 
 	listen, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
@@ -52,36 +60,52 @@ func listenClients(port int) error {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	buffer := make([]byte, 4096)
 
-	n, err := conn.Read(buffer)
+	// Comandos enviados pelo Cliente
+	// JOIN username\n
+	// MSG message...\n
+	// VIEW\n
+
+	scanner := bufio.NewScanner(conn)
+	scanner.Split(bufio.ScanWords)
+
+	writer := bufio.NewWriter(conn)
+
+	scanner.Scan()
+	command := scanner.Text()
+
+	err := scanner.Err()
 
 	if err != nil {
 		log.Printf("ERRO em conexão com cliente: %v.", err)
 		return
 	}
 
-	command := string(buffer[:n])
-	command = strings.Trim(command, " ")
-
 	switch command {
 	case "JOIN":
-		log.Printf("Nova conexão do tipo JOIN iniciada.\n")
+		log.Printf("Nova conexão do tipo JOIN requisitada.\n")
 
-		n, err = conn.Read(buffer)
+		scanner.Scan()
+		name := scanner.Text()
+
+		err = scanner.Err()
 
 		if err != nil {
 			log.Printf("ERRO em conexão com cliente: %v", err)
+			writer.Write([]byte("ERR\n"))
+			writer.Flush()
 			break
 		}
-
-		name := strings.Trim(string(buffer[:n]), " ")
 
 		if len(name) == 0 {
 			log.Printf("Usuário enviou o nome vazio. Encerrando conexão.\n")
+			writer.Write([]byte("ERR\n"))
+			writer.Flush()
 			break
 		}
 
+		writer.Write([]byte("OK\n"))
+		writer.Flush()
 		log.Printf("Nova conexão do tipo JOIN feita por %s\n", name)
 
 		msg := fmt.Sprintf("%s entrou no chat.", name)
@@ -90,24 +114,42 @@ func handleConnection(conn net.Conn) {
 
 		// loop para esperar por mensagens desse usuario
 		for {
-			n, err = conn.Read(buffer)
+			scanner.Scan()
+			command = scanner.Text()
+
+			err = scanner.Err()
 			if err != nil {
 				log.Printf("ERRO em conexão com cliente: %v.", err)
+				writer.Write([]byte("ERR\n"))
+				writer.Flush()
+				break
+			}
+
+			if command != "MSG" {
+				log.Printf("ERRO em conexão com cliente: era esperado comando MSG.")
+				writer.Write([]byte("ERR\n"))
+				writer.Flush()
 				break
 			}
 
 			// recebe mensagem e trata espaços em branco
-			msg = string(buffer[:n])
-			msg = strings.Trim(msg, " ")
+			tokens := make([]string, 0)
 
-			// verifica se é mensagem vazia ou comando quit
-			if n == 0 || msg == "quit" {
-				// encerra loop para desconectar usuario
-				break
+			for scanner.Scan() {
+				if scanner.Text() == "<end>" {
+					break
+				}
+
+				tokens = append(tokens, scanner.Text())
 			}
+
+			msg = strings.Join(tokens, " ")
 
 			// adiciona mensagem na fila e volta a esperar novas mensagens
 			AddMessage(name, msg)
+
+			writer.Write([]byte("OK\n"))
+			writer.Flush()
 		}
 
 		msg = fmt.Sprintf("%s saiu do chat.", name)
@@ -118,26 +160,41 @@ func handleConnection(conn net.Conn) {
 	case "VIEW":
 		log.Printf("Nova conexao do tipo VIEW\n")
 		var i = 0
-		var msg string
 
 		for {
+			if !connectAlive(conn) {
+				break
+			}
+
 			// se houver novas mensagens, envia para o cliente
 			for i < len(messageQueue) {
-				msgQueueMutex.Lock()
-				msg = fmt.Sprintf("%s: %s\n", messageQueue[i].SenderName, messageQueue[i].Text)
-				msgQueueMutex.Unlock()
-				conn.Write([]byte(msg))
+				writer.Write([]byte(
+					fmt.Sprintf("%v\n", messageQueue[i])))
 				i++
 			}
+			writer.Flush()
 		}
 	}
 
+}
+
+func connectAlive(conn net.Conn) bool {
+	conn.SetReadDeadline(time.Now())
+	var one []byte
+
+	if _, err := conn.Read(one); err == io.EOF {
+		return false
+	} else {
+		var zero time.Time
+		conn.SetReadDeadline(zero)
+		return true
+	}
 }
 
 func main() {
 	const PORT = 7777
 	err := listenClients(PORT)
 	if err != nil {
-		log.Fatal("ERRO: %v\n", err)
+		log.Fatal("Erro fatal: %v\n", err)
 	}
 }
